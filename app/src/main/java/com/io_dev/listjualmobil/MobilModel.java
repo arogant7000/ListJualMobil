aa1bcb7ecfe50fe687c70bfc340859818c05e4f5
arogant7000 <arogant7000@gmail.com>
2017-01-30 01:52:29 +0700

    Initial commit

---
+++ app/src/main/java/com/io_dev/listjualmobil/MobilModel.java
@@ -0,0 +1,43 @@
+package com.io_dev.listjualmobil;
+
+import java.io.Serializable;
+
+/**
+ * Created by arogant7000 on 1/29/17.
+ */
+
+public class MobilModel implements Serializable {
+    String title, image, harga, lokasi;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+
+    public String getLokasi() {
+        return lokasi;
+    }
+
+    public void setLokasi(String lokasi) {
+        this.lokasi = lokasi;
+    }
+}
